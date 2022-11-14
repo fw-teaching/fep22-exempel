bbbeee22f8230fbc59c182419ccc6d4481ca3939
Fredde <welandfr@arcada.fi>
2022-11-14 15:55:50 +0200

    Lektion 4

--- lektion-4/timers.js
+++ lektion-4/timers.js
@@ -1,5 +1,6 @@
 console.log("timers.js INIT")
 
+/*
 // anonym callback-funktionen direkt i setInterval
 let count = 0;
 setInterval(function () {
@@ -14,14 +15,35 @@ function timerAction() {
     count2--;
 }
 setInterval(timerAction, 1000);
-
+*/
 // Namnge setInterval så att du kommer åt den senare
 let count3 = 0;
 const myTimer = setInterval(function () {
     console.log('Hello3 !!! ', count3);
     count3++;
 }, 1000)
-// funktion för att hantera avstängning av myTimer
-function stopHello3() {
+
+// Lyssnare för knapp (Event Listener)
+document.querySelector('#stop-hello').addEventListener('click', function (evt) {
+    console.log(`Du klickade på ${evt.target.tagName} ${evt.target.id}`);
     clearInterval(myTimer);
-}
+});
+
+
+
+
+
+
+// Date() utan argument ger tiden just nu.
+
+setInterval(function () {
+    const nu = new Date();
+    document.querySelector('#klocka').innerText = `
+        Klockan är ${nu.getHours()}:${nu.getMinutes()}:${nu.getSeconds()}
+    `;
+}, 1000);
+
+// OBS månaden 0-11: 11 = december!
+const juldag = new Date(2022, 11, 25, 15, 30);
+console.log(juldag)
+
