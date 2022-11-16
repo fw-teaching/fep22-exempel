4f2d3b34d03fbc8ef31f4267dcd92d5a53933d6a
Fredde <welandfr@arcada.fi>
2022-11-16 11:58:36 +0200

    Lektion 5

--- lektion-5/script.js
+++ lektion-5/script.js
@@ -10,4 +10,29 @@ console.log("id för vår ul: ", document.querySelector('#colors').getAttribute(
 // Loopa igenom flera element som identifieras med selector 
 document.querySelectorAll('#colors li').forEach((elem) => {
     console.log(elem.getAttribute('data-hex'));
+
+    elem.addEventListener('click', (evt) => {
+        elem.style.backgroundColor = 'coral';
+
+        document.querySelectorAll('#colors li').forEach((elem2) => {
+
+            if (elem2 != evt.target) {
+                elem2.style.backgroundColor = 'silver';
+            }
+        })
+
+    });
 })
+
+// Skapa lyssnare på container-element (parent-element)
+document.querySelector('#colors').addEventListener('mouseover', (evt) => {
+    console.log(evt)
+    if (evt.target.id == "green") {
+        console.log("supergreen!");
+    }
+});
+
+// Skapa lyssnare på container-element (parent-element)
+document.querySelector('#slider').addEventListener('input', (evt) => {
+    console.log(evt.target.value);
+});
